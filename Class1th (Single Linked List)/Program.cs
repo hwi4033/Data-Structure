47709b8e0cf66e82d7d02d19aa087ceb74948fd9
hwi4033 <ckdgnl6381@naver.com>
2024-06-11 15:44:10 +0900

    2024.06.11 양방향 연결 리스트

---
+++ Class1th (Single Linked List)/Program.cs
@@ -0,0 +1,148 @@
+﻿namespace Class1th__Single_Linked_List_
+{
+    public class SingleLinkedList<T>
+    {
+        private class Node
+        {
+            public T data;
+            public Node next;
+        }
+
+        private Node head;
+        private int size;
+
+        public SingleLinkedList()
+        {
+            size = 0;
+            head = null;
+        }
+
+        public int Size
+        {
+            get { return size; }
+        }
+
+        public void PushFront(T data)
+        {
+            if (head == null)
+            {
+                head = new Node();
+                head.data = data;
+                head.next = null;
+            }
+            else
+            {
+                Node newNode = new Node();
+                newNode.data = data;
+                newNode.next = head;
+                head = newNode;
+            }
+
+            size++;
+        }
+
+        public void PushBack(T data)
+        {
+            if (head == null)
+            {
+                head = new Node();
+                head.data = data;
+                head.next = null;
+            }
+            else
+            {
+                Node currentNode = head;
+
+                if (currentNode.next == null)
+                {
+                    Node newNode = new Node();
+
+                    newNode.data = data;
+                    currentNode.next = newNode;
+                }
+                else
+                {
+                    while (currentNode.next != null)
+                    {
+                        currentNode = currentNode.next;
+                    }
+
+                    Node newNode = new Node();
+                    newNode.data = data;
+                    currentNode.next = newNode;
+                }
+            }
+
+            size++;
+        }
+
+        public void Show()
+        {
+            Node currentNode = head;
+            while (currentNode != null)
+            {
+                Console.WriteLine(currentNode.data);
+
+                currentNode = currentNode.next;
+            }
+        }
+
+        public void RemoveFront()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Linked List is Empty");
+            }
+            else
+            {
+                head = head.next;
+
+                size--;
+            }
+        }
+
+        public void RemoveBack()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Linked List is Empty");
+            }
+            else
+            {
+                if (size == 1)
+                {
+                    head = null;
+                }
+                else
+                {
+                    Node currentNode = head;
+                    Node previousNode = currentNode;
+
+                    while (currentNode.next != null)
+                    {
+                        previousNode = currentNode;
+                        currentNode = currentNode.next;
+                    }
+
+                    previousNode.next = currentNode.next;
+                }
+
+                size--;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // SingleLinkedList<int> singlelinkedlist = new SingleLinkedList<int>();
+            // singlelinkedlist.PushFront(10);
+            // singlelinkedlist.PushBack(20);
+            // singlelinkedlist.RemoveBack();
+            // 
+            // Console.WriteLine("size : " + singlelinkedlist.Size);
+            // singlelinkedlist.Show();
+        }
+    }
+}
--- Program/Program.cs
+++ Program/Program.cs
@@ -3,21 +3,24 @@ using System.Xml.Linq;
 
 namespace Program
 {
-    public class SingleLinkedList<T>
+    public class DoubleLinkedList<T>
     {
-        private class Node
+        public class Node
         {
             public T data;
+            public Node previous;
             public Node next;
         }
 
-        private Node head;
         private int size;
+        private Node head;
+        private Node tail;
 
-        public SingleLinkedList()
+        public DoubleLinkedList()
         {
             size = 0;
             head = null;
+            tail = null;
         }
 
         public int Size
@@ -25,20 +28,28 @@ namespace Program
             get { return size; }
         }
 
-        public void PushFront(T data)
+        public void PushBack(T data)
         {
-            if (head == null)
-            {
-                head = new Node();
-                head.data = data;
-                head.next = null;
+            Node newNode = new Node();
+
+            if (tail == null)
+            {                
+                head = newNode;
+                tail = newNode;
+
+                newNode.data = data;
+                newNode.next = null;
+                newNode.previous = head;
             }
             else
             {
-                Node newNode = new Node();
+                tail.next = newNode;
+                newNode.previous = tail;
+
+                tail = newNode;
+
                 newNode.data = data;
-                newNode.next = head;
-                head = newNode;
+                newNode.next = null;
             }
 
             size++;
@@ -52,22 +63,21 @@ namespace Program
                 Console.WriteLine(currentNode.data);
 
                 currentNode = currentNode.next;
-            }           
+            }
         }
-
     }
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            SingleLinkedList<int> singlelinkedlist = new SingleLinkedList<int>();
-            singlelinkedlist.PushFront(30);
-            singlelinkedlist.PushFront(20);
-            singlelinkedlist.PushFront(10);
+            DoubleLinkedList<int> doubleLinkedList = new DoubleLinkedList<int>();
+            doubleLinkedList.PushBack(10);
+            doubleLinkedList.PushBack(20);
+            doubleLinkedList.PushBack(30);
 
-            singlelinkedlist.Show();
-            Console.WriteLine("size : " + singlelinkedlist.Size);
+            Console.WriteLine("size : " + doubleLinkedList.Size);
+            doubleLinkedList.Show();
         }
     }
 }
