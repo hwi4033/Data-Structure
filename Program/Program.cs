0c98cc8a9d3a121499b4cc557c282f03867b3ac8
hwi4033 <ckdgnl6381@naver.com>
2024-06-12 15:45:33 +0900

    2024.06.12 원형 연결 리스트

--- Program/Program.cs
+++ Program/Program.cs
@@ -28,6 +28,42 @@ namespace Program
             get { return size; }
         }
 
+        public Node First()
+        {
+            return head;
+        }
+
+        public Node Last()
+        {
+            return tail;
+        }
+
+        public void PushFront(T data)
+        {
+            if (head == null)
+            {               
+                head = new Node();
+                tail = head;
+
+                head.data = data;
+                head.next = null;
+                head.previous = null;
+            }
+            else
+            {
+                Node newNode = new Node();
+                newNode.next = head;
+
+                head.previous = newNode;
+                head = newNode;
+
+                newNode.data = data;
+                newNode.previous = null;
+            }
+
+            size++;
+        }
+
         public void PushBack(T data)
         {
             Node newNode = new Node();
@@ -55,6 +91,91 @@ namespace Program
             size++;
         }
 
+        public void RemoveFront()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Linked List is Empty");
+            }
+            else
+            {
+                if (head == tail)
+                {   // size == 1 (O)
+                    head = head.previous;
+                    tail = head;
+                }
+                else
+                {
+                    head.next.previous = null;
+                    head = head.next;
+                }
+
+                size--;
+            }
+        }
+
+        public void RemoveBack()
+        {
+            if (tail == null)
+            {
+                Console.WriteLine("Linked List is Empty");
+            }
+            else
+            {
+                if (size == 1)
+                {   // head == tail (O)
+                    tail = tail.next;
+                    head = tail;
+                }
+                else
+                {
+                    tail.previous.next = null;
+                    tail = tail.previous;
+                }
+
+                size--;
+            }
+        }
+
+        public void AddAfter(Node position, T data)
+        {
+            if (position == tail)
+            {
+                PushBack(data);
+            }
+            else
+            {
+                if (position == head)
+                {
+                    Node previousNode = head;
+                    Node nextNode = head.next;
+                    Node newNode = new Node();
+
+                    previousNode.next = newNode;
+                    newNode.previous = previousNode;
+                    nextNode.previous = newNode;
+                    newNode.next = nextNode;
+
+                    newNode.data = data;
+                }
+                else
+                {
+                    Node previousNode = position;
+                    Node nextNode = position.next;
+                    Node newNode = new Node();
+
+                    previousNode.next = newNode;
+                    newNode.previous = previousNode;
+                    nextNode.previous = newNode;
+                    newNode.next = nextNode;
+
+                    newNode.data = data;
+                }
+
+                size++;
+            }
+        }
+
         public void Show()
         {
             Node currentNode = head;
@@ -72,9 +193,14 @@ namespace Program
         static void Main(string[] args)
         {
             DoubleLinkedList<int> doubleLinkedList = new DoubleLinkedList<int>();
-            doubleLinkedList.PushBack(10);
-            doubleLinkedList.PushBack(20);
-            doubleLinkedList.PushBack(30);
+
+            DoubleLinkedList<int>.Node node = doubleLinkedList.First();
+
+            doubleLinkedList.PushFront(30);
+            doubleLinkedList.PushFront(20); 
+            doubleLinkedList.PushFront(10);
+
+            doubleLinkedList.AddAfter(node, 5);
 
             Console.WriteLine("size : " + doubleLinkedList.Size);
             doubleLinkedList.Show();
