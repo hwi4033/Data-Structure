7372dcc894010562e16cbbbdc6c86fdacd9c7139
hwi4033 <ckdgnl6381@naver.com>
2024-06-14 15:46:53 +0900

    2024.06.14 선형 큐

---
+++ Class3th (Circle Linked List)/Program.cs
@@ -0,0 +1,144 @@
+﻿namespace Class3th__Circle_Linked_List_
+{
+    public class CircleLinkedList<T>
+    {
+        public class Node
+        {
+            public T data;
+            public Node next;
+        }
+
+        private int size;
+        private Node head;
+
+        public CircleLinkedList()
+        {
+            size = 0;
+            head = null;
+        }
+
+        public int Size
+        {
+            get { return size; }
+        }
+
+        public void PushFront(T data)
+        {
+            Node newNode = new Node();
+            newNode.data = data;
+
+            if (head == null)
+            {
+                head = newNode;
+                newNode.next = head;
+            }
+            else
+            {
+                newNode.next = head.next;
+                head.next = newNode;
+            }
+
+            size++;
+        }
+
+        public void PushBack(T data)
+        {
+            Node newNode = new Node();
+            newNode.data = data;
+
+            if (head == null)
+            {
+                head = newNode;
+                newNode.next = head;
+            }
+            else
+            {
+                newNode.next = head.next;
+                head.next = newNode;
+                head = newNode;
+            }
+
+            size++;
+        }
+
+        public void RemoveFront()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("No Data Exists");
+            }
+            else
+            {
+                if (head == head.next)
+                {
+                    head = null;
+                }
+                else
+                {
+                    head.next = head.next.next;
+                }
+
+                size--;
+            }
+        }
+
+        public void RemoveBack()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("No Data Exists");
+            }
+            else
+            {
+                if (head == head.next)
+                {
+                    head = null;
+                }
+                else
+                {
+                    Node currentNode = head;
+
+                    for (int i = 0; i < size - 1; i++)
+                    {
+                        currentNode = currentNode.next;
+                    }
+
+                    currentNode.next = head.next;
+                    head = currentNode;
+                }
+
+                size--;
+            }
+        }
+
+        public void Show()
+        {
+            if (head != null)
+            {
+                Node currentNode = head.next;
+
+                for (int i = 0; i < size; i++)
+                {
+                    Console.WriteLine(currentNode.data);
+
+                    currentNode = currentNode.next;
+                }
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CircleLinkedList<int> circleLinkedList = new CircleLinkedList<int>();
+            circleLinkedList.PushFront(30);
+            circleLinkedList.PushFront(20);
+            circleLinkedList.PushFront(10);
+
+            circleLinkedList.RemoveBack();
+
+            Console.WriteLine("size : " + circleLinkedList.Size);
+            circleLinkedList.Show();
+        }
+    }
+}
--- Program/Program.cs
+++ Program/Program.cs
@@ -1,96 +1,138 @@
-﻿using System.Net.Http.Headers;
-using System.Security.Cryptography.X509Certificates;
-using System.Xml.Linq;
-
-namespace Program
+﻿namespace Program
 {
-    public class CircleLinkedList<T>
-    {
-        public class Node
+    public class Stack<T>
+    { 
+        private int top;
+        private T error;
+        private T[] array;
+        private readonly int arraySize;
+
+        public Stack()
         {
-            public T data;
-            public Node next;
+            top = -1;
+            arraySize = 10;
+            array = new T[arraySize];
         }
 
-        private int size;
-        private Node head;
-
-        public CircleLinkedList()
+        public void Push(T data)
         {
-            size = 0;
-            head = null;
-        }
-
-        public int Size
-        {
-            get { return size; }
-        }
-
-        public void PushFront(T data)
-        {
-            Node currentNode = head.next;
-            Node newNode = new Node();
-
-            if (head == null)
+            if (top >= arraySize - 1)
             {
-                head = newNode;
-                newNode.next = head;
-
-                newNode.data = data;
+                Console.WriteLine("Stack Overflow");
             }
             else
             {
-                head.next = newNode;
-                newNode.next = currentNode;
-
-                newNode.data = data;
+                array[++top] = data;
             }
         }
 
-        public void PushBack(T data)
+        public T Pop()
         {
-            Node newNode = new Node();
-            newNode.data = data;
-
-            if (head == null)
+            if (top <= -1)
             {
-                head = newNode;
-                newNode.next = head;
+                Console.WriteLine("Stack is Empty");
+                return error;
             }
             else
             {
-                newNode.next = head.next;
-                head.next = newNode;
-                head = newNode;            
+                return array[top--];
             }
-
-            size++;
         }
 
-        public void Show()
+        public T Peek()
         {
-            Node currentNode = head.next;
-
-            do
+            if (top <= -1)
             {
-                Console.WriteLine(currentNode.data);
+                Console.WriteLine("Stack is Empty");
+                return error;
+            }
+            else
+            {
+                return array[top];
+            }
+        }
 
-                currentNode = currentNode.next;
-            } while (currentNode != head.next);
+        public bool Constains(T data)
+        {
+            for (int i = 0; i <= top; i++)
+            {
+                if (array[i].ToString() == data.ToString())
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
+
     internal class Program
     {
+        static bool CheckBracket(string content)
+        {
+            if (content.Length <= 0)
+            {
+                return false;
+            }
+            int temp = 0;
+            Stack<char> stack = new Stack<char>();
+            
+            for (int i = 0; i <= content.Length / 2; i++)
+            {
+                stack.Push(content[i]);
+            }
+
+            for (int i = 0; i <= content.Length / 2; i++)
+            {
+                for (int j = content.Length / 2 + 1; j <= content.Length; j++)
+                {
+                    switch (stack.Peek())
+                    {
+                        case '{':
+                            if (content[j] == '}')
+                            {
+                                stack.Pop();
+                                break;
+                            }
+                            else temp = 1;
+                            break;
+                        case '(':
+                            if (content[j] == ')')
+                            {
+                                stack.Pop();
+                                break;
+                            }
+                            else temp = 1;
+                            break;
+                        case '[':
+                            if (content[j] == ']')
+                            {
+                                stack.Pop();
+                                break;
+                            }
+                            else temp = 1;
+                            break;
+                    }
+                }
+            }
+
+            if (temp != 1) return true;
+            else return false;
+        }
+
         static void Main(string[] args)
         {
-            CircleLinkedList<int> circleLinkedList = new CircleLinkedList<int>();
-
-            circleLinkedList.PushBack(10);
-            circleLinkedList.PushBack(20);
-            circleLinkedList.PushBack(30);
-
-            Console.WriteLine("size : " + circleLinkedList.Size);
-            circleLinkedList.Show();
+            // Stack<int> stack = new Stack<int>();
+            // stack.Push(10);
+            // stack.Push(20);
+            // stack.Push(30);
+            // stack.Push(40);
+            // stack.Push(50);
+            // stack.Pop();
+            // 
+            // Console.WriteLine("Stack의 Peek : " + stack.Peek());
+            // Console.WriteLine(stack.Constains(99));
+            Console.WriteLine(CheckBracket("{{{[(((}))]}}}"));
         }
     }
 }
