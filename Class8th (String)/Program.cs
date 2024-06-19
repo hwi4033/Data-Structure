eeb90ab7d9670c49d6775648fa8e6f0e6e519e70
hwi4033 <ckdgnl6381@naver.com>
2024-06-19 15:41:53 +0900

    2024.06.19 문자열

---
+++ Class8th (String)/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace Class8th__String_
+{
+    public class String
+    {
+        private char[] array;
+
+        public String()
+        {
+            array = null;
+        }
+
+        public int Size()
+        {
+            return array.Length - 1;
+        }
+
+        public void Add(char[] content)
+        {
+            array = new char[content.Length + 1];
+
+            for (int i = 0; i < content.Length; i++)
+            {
+                array[i] = content[i];
+            }
+        }
+
+        public void Concat(char[] content)
+        {
+            char[] newArray = new char[(array.Length - 1) + content.Length + 1];
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                newArray[i] = array[i];
+            }
+
+            for (int i = 0; i < content.Length; i++)
+            {
+                newArray[(array.Length - 1) + i] = content[i];
+            }
+
+            array = newArray;
+        }
+
+        public void Show()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+            }
+
+            Console.WriteLine();
+        }
+
+        public int IndexOf(char alphabet)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == alphabet)
+                {
+                    return i;
+                }
+            }
+
+            return -99999;
+        }
+
+        public bool Equals(char[] content)
+        {
+            if (array == null)
+            {
+                return false;
+            }
+            else if (array.Length - 1 != content.Length)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i] != content[i])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = new String();
+
+            name.Add(new char[] { 'A', 'B', 'C', 'R' });
+
+            name.Show();
+            Console.WriteLine(name.IndexOf('R'));
+            Console.WriteLine(name.Equals(new char[] { 'A', 'B', 'C', 'R' }));
+            Console.WriteLine("Size : " + name.Size());
+        }
+    }
+}
--- Program/Program.cs
+++ Program/Program.cs
@@ -1,111 +1,14 @@
 ﻿using System.Data.Common;
+using System.Linq.Expressions;
+using System.Xml.Schema;
 
 namespace Program
 {
-    public class Vector<T>
-    {
-        private int size;
-        private int capacity;
-
-        private T[] array;
-        
-        public Vector()
-        {
-            size = 0;
-            capacity = 0;
-
-            array = null;
-        }
-
-        public T this [int index]
-        {
-            get { return array[index]; }
-        } 
-
-        public int Count()
-        {
-            return size;
-        }
-
-        public void Reserve(int newSize)
-        {
-            if (capacity > newSize)
-            {
-                return;
-            }
-            else
-            {
-                Resize(newSize);
-            }
-        }
-
-        public void Resize(int newSize)
-        {
-            capacity = newSize;
-            T[] newArray = new T[capacity];
-                          
-            for (int i = 0; i < size; i++)
-            {
-                newArray[i] = array[i];
-            }
-
-            array = newArray;
-        }
-
-        public void Add(T data)
-        {
-            if (capacity <= 0)
-            {
-                Resize(1);
-            }
-            else if (size >= capacity)
-            {
-                Resize(capacity * 2);
-            }
-
-            array[size++] = data;
-        }
-
-        public void RemoveAt(int index)
-        {
-            if (index > size)
-            {
-                Console.WriteLine("Error");
-            }
-            else
-            {
-                for (int i = index; i < size; i++)
-                {
-                    if (i == size - 1)
-                    {
-                        array[i] = default;
-                        size--;
-                        break;
-                    }
-
-                    array[i] = array[i + 1];
-                }
-            }
-        }
-    }
-
     internal class Program
     {
         static void Main(string[] args)
         {
-            Vector<int> vector = new Vector<int>();
-
-            vector.Reserve(4);
-            vector.Add(10);
-            vector.Add(20);
-            vector.Add(30);
-            vector.Add(40);
-            vector.RemoveAt(3);
-
-            for (int i = 0; i < vector.Count(); i++)
-            {
-                Console.WriteLine(vector[i]);
-            }
+            
         }
     }
 }
